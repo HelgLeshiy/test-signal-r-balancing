42f822d14ecf5beb71f2c0d90e94f97133f17b16
Belov Oleg <belov@intellex.ru>
2024-10-13 20:37:22 +0300

    oups, forgot \n

--- test-signal-r-balancing/Controllers/UsageController.cs
+++ test-signal-r-balancing/Controllers/UsageController.cs
@@ -14,11 +14,11 @@ namespace TestSignalRBalancing.Controllers
             int counter = counterService_.Counter;
             if(counter > 7200.0)
             {
-                return Ok("up 0%");
+                return Ok("up 0%\n");
             }
             else
             {
-                return Ok("up " + ((int)Math.Floor(100 * ( 1.0 - counter / 7200.0 ))).ToString() + "%");
+                return Ok("up " + ((int)Math.Floor(100 * ( 1.0 - counter / 7200.0 ))).ToString() + "%\n");
             }
         }
     }

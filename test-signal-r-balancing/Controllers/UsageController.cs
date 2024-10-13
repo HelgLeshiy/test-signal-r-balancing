96324ace1a903bd96d0b003578aab078583f492b
Belov Oleg <belov@intellex.ru>
2024-10-13 20:32:41 +0300

    metrics add "up"

--- test-signal-r-balancing/Controllers/UsageController.cs
+++ test-signal-r-balancing/Controllers/UsageController.cs
@@ -14,11 +14,11 @@ namespace TestSignalRBalancing.Controllers
             int counter = counterService_.Counter;
             if(counter > 7200.0)
             {
-                return Ok("0%");
+                return Ok("up 0%");
             }
             else
             {
-                return Ok(((int)Math.Floor(100 * ( 1.0 - counter / 7200.0 ))).ToString() + "%");
+                return Ok("up " + ((int)Math.Floor(100 * ( 1.0 - counter / 7200.0 ))).ToString() + "%");
             }
         }
     }
